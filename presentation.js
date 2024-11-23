c41901600d7e731e6ac6d899acb27d69592ef546
RanimElhafy <60097726@udst.edu.qa>
2024-11-23 11:51:22 +0300

    I added the view engine setup and routes configuration for handling different functionalities in the application

---
+++ presentation.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const handlebars = require("express-handlebars");
+const cookieParser = require("cookie-parser");
+const app = express();
+const path = require("path");
+

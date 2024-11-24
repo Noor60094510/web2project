cf9d43621841bda5663066d0f910e85f7be31786
Noora <60094510@udst.edu.qa>
2024-11-24 20:16:26 +0300

    added a file called models that has chat.js and user.js inside the chat.js i added in the chat.js a code that defines a mongoose schema

---
+++ models/chat.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const chatSchema = new mongoose.Schema({
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  receiverId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  message: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Chat", chatSchema);
